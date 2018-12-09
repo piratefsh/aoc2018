82660849d893365a460fe138315ad6c31d9b9a9a
Sher Minn Chong <sherminn.chong@gmail.com>
2018-12-08 19:59:43 -0500

    Cleanup

--- 7/solution.py
+++ 7/solution.py
@@ -17,7 +17,7 @@ class Node:
     self.afters.append(node)
 
   def ready(self):
-    # has no befores, or no unvisited befores
+    # is not already occupied and has no befores, or no unvisited befores
     return not self.occupied and (len(self.befores) == 0 or len([b for b in self.befores if not b.visited]) == 0)
 
   def occupy(self):
@@ -42,6 +42,63 @@ class Node:
     afts = ", ".join([a.name for a in self.afters])
     return "<%s befores: %s, afters: %s>" % (self.name, befs, afts)
 
+class Worker:
+  def __init__(self, wid, queue, done_queue):
+    self.busy = False
+    self.id = wid
+    self.duration = 0
+    self.curr_node = None
+    self.queue = queue
+    self.done_queue = done_queue
+
+  def occupy(self, node):
+    node.occupy()
+    self.busy = True
+    self.curr_node = node
+    self.duration = node.duration
+
+  def finish_node(self, n):
+    n.visit()
+    self.done_queue.append(n.name)
+
+  def tick(self):
+    self.duration -= 1
+
+    # if done with work, free self
+    if self.duration == 0:
+      self.busy = False
+      self.finish_node(self.curr_node)
+
+  def __repr__(self):
+    return "Worker %d" % self.id
+
+def order(nodes, workers=1):
+  done_queue = []
+  total_nodes = len(nodes.values())
+
+  # sort nodes
+  queue = sorted([n for n in nodes.values()])
+
+  # create workers
+  workers = [Worker(i, queue, done_queue) for i in range(workers)]
+  ticks = 0
+
+  while len(done_queue) != total_nodes:
+    for n in queue:
+      # if node is ready for use
+      if n.ready():
+        # if has available workers,
+        avail_workers = [w for w in workers if not w.busy]
+        if len(avail_workers) > 0:
+          # put someone to work
+          avail_workers[0].occupy(n)
+
+    # advance time
+    [w.tick() for w in workers]
+    ticks += 1
+
+  return "".join(done_queue), ticks
+
 def parse(file, base_duration=0):
   nodes = {}
   for line in file:
@@ -57,63 +114,6 @@ def parse(file, base_duration=0):
 
   # print(nodes.values())
   return nodes
-
-class Worker:
-  def __init__(self, wid, queue, done_queue):
-    self.busy = False
-    self.id = wid
-    self.duration = 0
-    self.curr_node = None
-    self.queue = queue
-    self.done_queue = done_queue
-
-  def occupy(self, node):
-    print(str(self) + ' occupied with ' + node.name)
-    node.occupy()
-    self.busy = True
-    self.curr_node = node
-    self.duration = node.duration
-
-  def finish_node(self, n):
-    print(n.name + ' finished')
-    n.visit()
-    self.done_queue.append(n.name)
-    # for a in n.afters:
-    #   if not a.visited and a not in self.queue:
-    #     bisect.insort_left(self.queue, a)
-
-  def tick(self):
-    self.duration -= 1
-    if self.duration == 0:
-      self.busy = False
-      self.finish_node(self.curr_node)
-      print(str(self) + ' freed')
-
-  def __repr__(self):
-    return "Worker %d" % self.id
-
-def order(nodes, workers=1):
-  done_queue = []
-  total_nodes = len(nodes.values())
-  queue = sorted([n for n in nodes.values()])
-  workers = [Worker(i, queue, done_queue) for i in range(workers)]
-  ticks = 0
-
-  while len(done_queue) != total_nodes:
-    for n in queue:
-      if n.ready():
-        # if has available workers,
-        avail_workers = [w for w in workers if not w.busy]
-        if len(avail_workers) > 0:
-          # put someone to work
-          avail_workers[0].occupy(n)
-
-    # advance time
-    [w.tick() for w in workers]
-    ticks += 1
-    # print('tick', ticks)
-
-  return "".join(done_queue), ticks
 
 nodes = parse(open('sample.txt'), 0)
 print(order(nodes, workers=2))
