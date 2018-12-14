45b68b927edc85cb7c8895e77dd865aff21619e4
Sher Minn Chong <sherminn.chong@gmail.com>
2018-12-13 22:08:15 -0500

    Cleanup

--- 12/solution.py
+++ 12/solution.py
@@ -1,3 +1,5 @@
+import math
+
 def parse(file):
   initial_state = file.readline().split(':')[1].strip()
   file.readline()
@@ -13,28 +15,27 @@ def parse(file):
 
   return [s == '#' for s in initial_state], conditionals
 
-def neighbours(i, state):
-  # if(i == 0):
-  #   return [False, False] + state[i: i+3]
-  # if(i == 1):
-  #   return [False] + state[i-1: i+3]
-  # if i == len(state) - 1:
-  #   return state[i - 2: i + 1] + [False, False]
-  # if i == len(state) - 2:
-  #   return state[i - 2: i + 2] + [False]
-
-  return state[i-2 : i + 3]
-
 def str_state(state):
   return "".join(['#' if c else '.' for c in state])
 
+def total(state, offset):
+  total = 0
+
+  for i in range(0, len(state)):
+    if state[i]:
+      total += (i - offset)
+  return total
+
 def run(state, conditionals, generations=20):
   curr_gen = [False] + state + [False]
+  tots = 0
+  delta = 0
+  seen_delta_count = 0
   offset = 1
   for g in range(generations):
     new_gen = [False] * len(curr_gen)
     for i in range(2, len(curr_gen) - 2):
-      nb = neighbours(i, curr_gen)
+      nb = curr_gen[i-2 : i + 3]
       for cond in conditionals:
         is_match, res = cond(nb)
         if(is_match):
@@ -42,20 +43,21 @@ def run(state, conditionals, generations=20):
           break
     offset += 1
     curr_gen = [False] + new_gen + [False]
+    new_tots = total(curr_gen, offset)
+    new_delta = new_tots - tots
 
-  total = 0
-  print('offset', offset)
-  for i in range(0, len(curr_gen)):
-    if curr_gen[i]:
-      total += (i - offset)
-  return total, str_state(curr_gen)
+    if(delta == new_delta):
+      seen_delta_count+=1
+    else:
+      seen_delta_count=0
+      delta = new_delta
+
+    # if seen new delta too many times, assume that it is always constant
+    if seen_delta_count > 20:
+      return (generations - g - 1) * delta + new_tots
+
+    tots = new_tots
+  return total(curr_gen, offset)
 
 init, conditionals = parse(open('input.txt'))
-# init, conditionals = parse(open('sample.txt'))
-# print(state, conditionals)
-# assert(neighbours(0, [True, True, True, False, False]) == [False, False, True, True, True])
-# assert(neighbours(1, [True, False, True, False, False]) == [False, True, False, True, False])
-# assert(neighbours(4, [False, False, True, True, True]) == [True, True, True, False, False])
-# assert(neighbours(3, [False, True, True, True, False]) == [True, True, True, False, False])
-# print(str_state(init))
-print(run(init, conditionals, generations=20))
+assert(run(init, conditionals, generations=5 * 10**9) == 400000001480)
