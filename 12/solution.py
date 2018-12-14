4875ff90c526224c86b1558a9629afc0de6eb273
Sher Minn Chong <sherminn.chong@gmail.com>
2018-12-13 21:45:20 -0500

    Cleanup

--- 12/solution.py
+++ 12/solution.py
@@ -14,14 +14,14 @@ def parse(file):
   return [s == '#' for s in initial_state], conditionals
 
 def neighbours(i, state):
-  if(i == 0):
-    return [False, False] + state[i: i+3]
-  if(i == 1):
-    return [False] + state[i-1: i+3]
-  if i == len(state) - 1:
-    return state[i - 2: i + 1] + [False, False]
-  if i == len(state) - 2:
-    return state[i - 2: i + 2] + [False]
+  # if(i == 0):
+  #   return [False, False] + state[i: i+3]
+  # if(i == 1):
+  #   return [False] + state[i-1: i+3]
+  # if i == len(state) - 1:
+  #   return state[i - 2: i + 1] + [False, False]
+  # if i == len(state) - 2:
+  #   return state[i - 2: i + 2] + [False]
 
   return state[i-2 : i + 3]
 
@@ -29,12 +29,11 @@ def str_state(state):
   return "".join(['#' if c else '.' for c in state])
 
 def run(state, conditionals, generations=20):
-  curr_gen =  state
-  offset = 0
+  curr_gen = [False] + state + [False]
+  offset = 1
   for g in range(generations):
-    print(str_state(curr_gen))
     new_gen = [False] * len(curr_gen)
-    for i in range(0, len(curr_gen)):
+    for i in range(2, len(curr_gen) - 2):
       nb = neighbours(i, curr_gen)
       for cond in conditionals:
         is_match, res = cond(nb)
@@ -52,11 +51,11 @@ def run(state, conditionals, generations=20):
   return total, str_state(curr_gen)
 
 init, conditionals = parse(open('input.txt'))
-init, conditionals = parse(open('sample.txt'))
+# init, conditionals = parse(open('sample.txt'))
 # print(state, conditionals)
-assert(neighbours(0, [True, True, True, False, False]) == [False, False, True, True, True])
-assert(neighbours(1, [True, False, True, False, False]) == [False, True, False, True, False])
-assert(neighbours(4, [False, False, True, True, True]) == [True, True, True, False, False])
-assert(neighbours(3, [False, True, True, True, False]) == [True, True, True, False, False])
+# assert(neighbours(0, [True, True, True, False, False]) == [False, False, True, True, True])
+# assert(neighbours(1, [True, False, True, False, False]) == [False, True, False, True, False])
+# assert(neighbours(4, [False, False, True, True, True]) == [True, True, True, False, False])
+# assert(neighbours(3, [False, True, True, True, False]) == [True, True, True, False, False])
 # print(str_state(init))
 print(run(init, conditionals, generations=20))
