e9b472fa9b3081d3d6b83670af97ecb675b3f12f
Sher Minn Chong <sherminn.chong@gmail.com>
2018-12-16 17:45:17 -0500

    Refactor

--- 15/solution.py
+++ 15/solution.py
@@ -27,10 +27,17 @@ class Sprite(Entity):
   def __init__(self, stype, pos):
     Entity.__init__(self, stype);
     self.pos = pos
+    self.ap = 3
+    self.hp = 200
+    self.dead = False
+
+  def reduce_hp(self, hp):
+    self.hp -= hp
+    if self.hp <= 0:
+      self.dead = True
 
   def attack(self, other):
-    # todo
-    pass
+    other.reduce_hp(self.ap)
 
 def parse(file):
   sprites = []
@@ -58,8 +65,25 @@ def parse(file):
 def sort(entities):
   return sorted(entities, key=lambda x: x.pos)
 
+def bfs(sprite, grid, targets):
+  queue = [sprite]
+  found = dict()
+  while len(queue) > 1:
+    curr, dist = queue.pop(0)
+
+    if curr in targets:
+      targets.remove(curr)
+      found[curr] = dist
+
+    # add neighbours
+    neighbours = [(x, y+1), (x, y-1), (x+1, y), (x-1, y)]
+    queue += [grid[y][x] for x, y in neighbours if grid[y][x].is_open()]
+  return found
+
 def move(sprite, sprites, grid, open_squares):
-  # find distance to each open square
+  # find shortest distance to each open square
+    # do bfs to find all squares it can reach
+
 
   # sort by distance, then reading order
 
@@ -71,9 +95,10 @@ def find_open(curr, grid):
   # find N, S, E, W squares
   surroundings = [(x, y+1), (x, y-1), (x+1, y), (x-1, y)]
 
-  return [grid[i][j] for i, j in surroundings if grid[i][j].is_open()]
+  return [grid[j][i] for i, j in surroundings if grid[i][j].is_open()]
 
-def dist(a, b):
+# manhattan distance
+def mdist(a, b):
   x1, y1 = a
   x2, y2 = b
   return math.abs((x2-x1) + (y2-y1))
@@ -86,7 +111,7 @@ def turn(curr, sprites, grid):
   open_squares = [find_open(t, grid) for t in target]
 
   # find in_range_targets, i.e. targets one step away
-  in_range_targets = [t for t in targets if dist(s, curr.pos) <= 1]
+  in_range_targets = [t for t in targets if mdist(s, curr.pos) <= 1]
 
   if len(open_squares) < 1 and len(in_range_targets) < 1:
     # end turn
@@ -94,13 +119,14 @@ def turn(curr, sprites, grid):
 
   # if has in_range_target
   if len(in_range_targets) > 0:
-    # attack
-    target = sort(in_range_targets)[0]
+    # attack target with lowest hp in reading order sort
+    target = sorted(sort(in_range_targets), key=lambda x: x.hp)
     curr.attack(target)
     return True
   else:
     # move
     move(curr, sprites, grid, open_squares)
+
 
 def print_grid(grid):
   for row in grid:
@@ -110,14 +136,16 @@ def print_grid(grid):
 
 def run(grid, sprites):
   has_move = True
+
   while has_move:
     has_move = False
     for s in sprites:
       has_move = has_move or turn(c, sprites, grid)
 
+    # remove dead players
+    sprites = [s for s in sprites if not s.dead]
 
 grid, sprites = parse(open('sample1.txt'))
 print_grid(grid)
-print(find_open(sprites[0], grid))
-print(find_open(sprites[4], grid))
-print(find_open(sprites[5], grid))
+
+print(bfs(sprites[0], grid, [sprites[1]]))
