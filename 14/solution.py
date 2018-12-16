17bb3080a74f3dc60251eb3a8a630b5d83c9a1aa
Sher Minn Chong <sherminn.chong@gmail.com>
2018-12-15 23:33:49 -0500

    add part 2

--- 14/solution.py
+++ 14/solution.py
@@ -11,7 +11,29 @@ def solution(rounds):
     elf2 = (1 + elf2 + score2) % len(recipes)
 
   return recipes[rounds:rounds + 10]
+
+def solution2(target):
+  recipes = "37"
+  elf1 = 0
+  elf2 = 1
+  rounds = 1
+  while(not recipes[-len(target):] == target):
+    score1 = int(recipes[elf1])
+    score2 = int(recipes[elf2])
+    newr = score1 + score2
+    recipes = recipes + str(newr)
+    elf1 = (1 + elf1 + score1) % len(recipes)
+    elf2 = (1 + elf2 + score2) % len(recipes)
+    rounds += 1
+  return recipes.find(target)
+
+  return recipes[rounds:rounds + 10]
 assert(solution(9) == '5158916779')
+assert(solution2('01245') == 5)
+assert(solution2('59414') == 2018)
+assert(solution2('51589') == 9)
+assert(solution2('92510') == 18)
 assert(solution(5) == '0124515891')
 assert(solution(18) == '9251071085')
-print(solution(793061))
+print(solution2('793061'))
+# print(solution(793061))
