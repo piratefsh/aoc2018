cc15592662fc021c0db02eb09680dba36469e9b0
Sher Minn Chong <sherminn.chong@gmail.com>
2018-12-16 17:26:08 -0500

    Refactor

--- 15/solution.py
+++ 15/solution.py
@@ -1,4 +1,5 @@
 from enum import Enum
+import math
 
 class CellType(Enum):
   WALL = '#'
@@ -12,6 +13,10 @@ class Entity:
   def __init__(self, etype, occupant=None):
     self.type = etype
     self.occupant = occupant
+
+  def is_open(self):
+    return self.type is not CellType.WALL and self.occupant is None
+
   def __repr__(self):
     if self.occupant is None:
       return str(self.type.value)
@@ -22,7 +27,10 @@ class Sprite(Entity):
   def __init__(self, stype, pos):
     Entity.__init__(self, stype);
     self.pos = pos
-  pass
+
+  def attack(self, other):
+    # todo
+    pass
 
 def parse(file):
   sprites = []
@@ -59,29 +67,40 @@ def move(sprite, sprites, grid, open_squares):
   pass
 
 def find_open(curr, grid):
-  pass
+  x, y = curr.pos
+  # find N, S, E, W squares
+  surroundings = [(x, y+1), (x, y-1), (x+1, y), (x-1, y)]
+
+  return [grid[i][j] for i, j in surroundings if grid[i][j].is_open()]
+
+def dist(a, b):
+  x1, y1 = a
+  x2, y2 = b
+  return math.abs((x2-x1) + (y2-y1))
 
 def turn(curr, sprites, grid):
   #  find all targets
   targets = [s for s in sprites if s.type is not curr.type]
 
   # find open_squares in range of targets
-    # for t in target
-      # find N, S, E, W squares
-      # if sq is not occupied or wall
-      # is open
-  for t in target:
-    t
+  open_squares = [find_open(t, grid) for t in target]
 
-  # find in_range_targets
+  # find in_range_targets, i.e. targets one step away
+  in_range_targets = [t for t in targets if dist(s, curr.pos) <= 1]
 
-  # if no open_squares or no in_range_targets
+  if len(open_squares) < 1 and len(in_range_targets) < 1:
     # end turn
+    return False
 
-  # if has in_range_target,
+  # if has in_range_target
+  if len(in_range_targets) > 0:
     # attack
-
-  # else move
+    target = sort(in_range_targets)[0]
+    curr.attack(target)
+    return True
+  else:
+    # move
+    move(curr, sprites, grid, open_squares)
 
 def print_grid(grid):
   for row in grid:
@@ -99,3 +118,6 @@ def run(grid, sprites):
 
 grid, sprites = parse(open('sample1.txt'))
 print_grid(grid)
+print(find_open(sprites[0], grid))
+print(find_open(sprites[4], grid))
+print(find_open(sprites[5], grid))
