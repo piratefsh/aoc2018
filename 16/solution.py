3ce3df35e1e4c94cdd47dc4faf7247aa33aa7ccc
Sher Minn Chong <sherminn.chong@gmail.com>
2018-12-23 13:51:24 -0500

    Add day 16 part 2

--- 16/solution.py
+++ 16/solution.py
@@ -31,7 +31,9 @@ def parse(file):
       # consume newline
       line = file.readline()
     else:
-      program.append(line.strip())
+      ins = line.strip().split(' ')
+      if(len(ins) > 1):
+        program.append([int(i) for i in ins])
   return samples, program
 
 def addr(A, B, C, R):
@@ -73,29 +75,50 @@ OPCODES = [addr, addi, mulr, muli,
             gtrr, eqir, eqri, eqrr]
 
 def test(samples):
+  opcode_ids = [-1] * len(OPCODES)
   three_or_more = 0
   for sample in samples:
-    matches = 0
+    matches = []
+    o, a, b, c = sample.instruction
+
     for op in OPCODES:
-      o, a, b, c = sample.instruction
       before = copy(sample.before)
       op(a, b, c, before)
 
       if before == sample.after:
-        matches += 1
+        matches.append(op)
         if DEBUG:
           print(op.__name__)
-    if matches >= 3:
-      three_or_more += 1
 
-  return three_or_more
+    if len(matches) >= 3:
+      three_or_more +=1
 
+    if len(matches) == 0:
+      raise Exception('Unrecognized operation')
+    elif len(matches) == 1:
+      opcode_ids[o] = matches[0]
+    elif len(matches) > 1:
+      # eliminate founds
+      unfound = [m for m in matches if m not in opcode_ids]
+      if len(unfound) == 1:
+        opcode_ids[o] = unfound[0]
+
+  return three_or_more, opcode_ids
+
+def run(opcodes, instructions):
+  registers = [0, 0, 0, 0]
+  for ins in instructions:
+    o, a, b, c = ins
+    opcodes[o](a, b, c, registers)
+  return registers
 def main():
   # samples, program = parse(open('input.txt'))
   samples, program = parse(open('sample.txt'))
   samples, program = parse(open('input.txt'))
-  print(samples)
-  print(test(samples))
+  three_or_more, opcodes = test(samples)
+  assert(three_or_more == 646)
+  assert(run(opcodes, program)[0] == 681)
+  print('tests pass')
 
 main()
 
