b89d19a2b86b2b0afab1bbb4d506674644a8b509
Sher Minn Chong <sherminn.chong@gmail.com>
2018-12-25 10:36:39 -0500

    Add day 20

--- 20/solution.py
+++ 20/solution.py
@@ -6,21 +6,50 @@ Node = namedtuple('Node', 'value, children')
 def parse(file):
   c = file.read(1)
   root = None
-  curr = None
+  stack = []
+  curr_frame = []
   while c:
+    print(stack)
+    print(curr_frame)
+    print()
     if c == '^':
-      n = file.read(1)
-      root = Node(n, [])
-      curr = root
-    elif c == '$':
-      break
+      root = Node(c, [])
+      stack.append(root)
+    elif c == '$' or c == ')':
+      parents = stack.pop()
+      for p in parents:
+        for node in curr_frame:
+          p.children.append(node)
+      curr_frame = parents
+
+      if c == '$':
+        break
+    elif c == '(':
+      # save curr frame
+      stack.append(curr_frame)
+
+      # new frame
+      curr_frame = []
     elif c in 'NEWS':
-      node = Node(c, [])
-      _, children = curr
-      children.append(node)
-      curr = node
+      node = Node(c + consume_snippet(file), [])
+      curr_frame.append(node)
+
     c = file.read(1)
   return root
+
+def peek(file):
+  pos = file.tell()
+  peekv = file.read(1)
+  file.seek(pos)
+  return peekv
+
+def consume_snippet(file):
+  val = ''
+  c = None
+  while peek(file) in 'NEWS':
+    c = file.read(1)
+    val += c
+  return val
 
 def print_paths(node):
   queue = [(node, 0)]
@@ -29,5 +58,14 @@ def print_paths(node):
     print(' ' * depth + curr[0])
     for c in curr.children:
       queue.append((c, depth + 1))
+
 paths = parse(open('sample1.txt'))
+print_paths(paths)
+
+print(open('sample2.txt').readline())
+paths = parse(open('sample2.txt'))
+print_paths(paths)
+
+print(open('sample3.txt').readline())
+paths = parse(open('sample3.txt'))
 print_paths(paths)
